becb79450d98e9300e3e0e56e97aed234198797f
Tommy-Air <tommy@air.ru>
2025-08-29 22:04:26 +0300

    ttfs MVC almost done

--- bash/ttfs.sh
+++ bash/ttfs.sh
@@ -1,34 +1,64 @@
 #!/bin/bash
 set -e
-SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+TODO: --extract-tag-from-file
 
-# Initialize default values
-NOW=$(date +%Y-%m-%dT%H:%M:%S)
-YEAR=$(date +%Y)
-ORIGIN="inet"
-LOCATION="home"
-PERSON="me"
-TAGS="no-tag"
-FILENAME=""
-EXTENSION=""
+# Colours for output
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+YELLOW='\033[1;33m'
+BLUE='\033[0;36m'
+PURPLE='\033[0;35m'
+NC='\033[0m'
+
+function log() {
+    echo -e "${GREEN}$1${NC}"
+}
+
+function info() {
+    echo -e "${BLUE}$1${NC}"
+}
+
+function error() {
+    echo -e "${RED}$1${NC}"
+}
+
+# Variables
+SCRIPT_DIR=$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)
+ORIGIN=""
+LOCATION=""
+PERSON=""
+TAGS=""
+EXTRACT_TS_FILE=false
+EXTRACT_TS_PHOTO=false
 
 # Function to display help
 function show_help() {
-    echo "Usage:   $0 [OPTIONS] <single-file>"
-    echo "Example: $0 --origin iphone --location london --person wife --tags my-kids IMG_3832.JPG"
+    info "Usage:   $0 [OPTIONS] file1 file2 ... fileN"
+    info  "Example: $0 --src iphone --loc london --usr me --tags my-kids --extract-photo-ts --extract-file-ts 1.JPG 2.JPG 3.JPG"
     echo
-    echo "┌──────────────────┬────────────────────────────┬─────────┬─────────────────┐"
-    echo "│ Option           │ Description                │ Default │ Example         │"
-    echo "├──────────────────┼────────────────────────────┼─────────┼─────────────────┤"
-    echo "│ --origin ORIGIN  │ File origin (inet, fb, vk) │ inet    │ --origin iphone │"
-    echo "│ --location PLACE │ Place, city or country     │ home    │ --location work │"
-    echo "│ --person PERSON  │ Person or group of people  │ me      │ --person wife   │"
-    echo "│ --tags TAG1-TAG2 │ Dash-separated-tags        │ no-tag  │ --tags my-kids  │"
-    echo "│ --help           │ Show this help message     │         │ --help          │"
-    echo "│ --               │ End of options marker      │         │ --              │"
-    echo "└──────────────────┴────────────────────────────┴─────────┴─────────────────┘"
+    echo "┌────────────────────┬──────────────────────────────────────────────────────────────────┬────────────────────┐"
+    echo "│ Option             │ Description                                                      │ Example            │"
+    echo "├────────────────────┼──────────────────────────────────────────────────────────────────┼────────────────────┤"
+    echo "│ --src SOURCE       │ File source (inet, iphone, hdd, usb, fb, vk, twitter, etc.)      │ --src phone        │"
+    echo "│ --loc LOCATION     │ Location (home, work, college, la, nyc, paris, usa, china, etc.) │ --loc work         │"
+    echo "│ --usr PERSON       │ Person or group of people (me, wife, parents, colleagues, etc.)  │ --usr wife         │"
+    echo "│ --tags TAG1-TAG2   │ Dash-separated-tags (my-wedding-photos, my-dogs-video, etc.)     │ --tags my-kids     │"
+    echo "│ --extract-file-ts  │ Extract file creation time from the file system                  │ --extract-file-ts  │"
+    echo "│ --extract-photo-ts │ Extract photo creation time from the EXIF data (if exists)       │ --extract-photo-ts │"
+    echo "│ --help             │ Show this help message                                           │ --help             │"
+    echo "│ --                 │ End of options marker                                            │ --                 │"
+    echo "└────────────────────┴──────────────────────────────────────────────────────────────────┴────────────────────┘"
+    echo
+    echo 'If both "--extract-file-ts" and "--extract-photo-ts" specified, then tries to extract from photo, then from FS'
+    echo
 }
 
+# Check tools
+if ! command -v exif &> /dev/null
+then
+    error "Error: The 'exif' utility could not be found. Please install it via Homebrew: brew install exif"   # TODO: Linux?
+    exit 1
+fi
 
 # Show help if no arguments provided
 if [[ $# -eq 0 ]]; then
@@ -39,31 +69,34 @@ fi
 # Parse command line arguments
 while [[ $# -gt 0 ]]; do
     case $1 in
-        --origin)
+        --src)
             if [[ -n $2 && $2 != --* ]]; then
                 ORIGIN="$2"
                 shift 2
             else
-                echo "Error: --output requires a value" >&2
-                exit 1
+                error "Error: --src requires a value"
+                show_help
+                exit 2
             fi
             ;;
-        --location)
+        --loc)
             if [[ -n $2 && $2 != --* ]]; then
                 LOCATION="$2"
                 shift 2
             else
-                echo "Error: --location requires a value" >&2
-                exit 2
+                error "Error: --loc requires a value"
+                show_help
+                exit 3
             fi
             ;;
-        --person)
+        --usr)
             if [[ -n $2 && $2 != --* ]]; then
                 PERSON="$2"
                 shift 2
             else
-                echo "Error: --person requires a value" >&2
-                exit 3
+                error "Error: --usr requires a value"
+                show_help
+                exit 4
             fi
             ;;
         --tags)
@@ -71,9 +104,18 @@ while [[ $# -gt 0 ]]; do
                 TAGS="$2"
                 shift 2
             else
-                echo "Error: --tags requires a value" >&2
-                exit 4
+                error "Error: --tags requires a value"
+                show_help
+                exit 5
             fi
+            ;;
+        --extract-file-ts)
+            EXTRACT_TS_FILE=true
+            shift
+            ;;
+        --extract-photo-ts)
+            EXTRACT_TS_PHOTO=true
+            shift
             ;;
         --help)
             show_help
@@ -85,8 +127,8 @@ while [[ $# -gt 0 ]]; do
             break
             ;;
         --*)
-            echo "Error: Unknown option $1" >&2
-            exit 1
+            error "Error: Unknown option $1"
+            exit 6
             ;;
         *)
             # Non-option argument - treat as positional argument
@@ -95,30 +137,82 @@ while [[ $# -gt 0 ]]; do
     esac
 done
 
-# Extract remaining positional argument
-if [[ $# == 1 ]]; then
-    FILENAME="$1"
-    if [[ -s "$FILENAME" ]]; then                                      # -s = std non-empty file
-        EXTENSION="${FILENAME##*.}"
-        EXTENSION=$(echo "$EXTENSION" | tr '[:upper:]' '[:lower:]')    # toLowerCase
-    else
-        echo "Error: File is empty or missing: $1" >&2
-        show_help
-        exit 5
-    fi
-else
-    echo "Error: Too many arguments: $@" >&2
+# Check main flags
+if [[ -z "$ORIGIN" ]]; then
+    error "Error: --src required"
     show_help
-    exit 6
+    exit 7
 fi
 
-# Debug variables
-echo "  File:        $FILENAME"
-echo "  --origin:    $ORIGIN"
-echo "  --location:  $LOCATION"
-echo "  --person:    $PERSON"
-echo "  --tags:      $TAGS"
+if [[ -z "$LOCATION" ]]; then
+    error "Error: --loc required"
+    show_help
+    exit 8
+fi
 
-# Main Logic
-mkdir -p "$SCRIPT_DIR/$YEAR"
-mv -v "$FILENAME" "$SCRIPT_DIR/$YEAR/${NOW}_${ORIGIN}_${LOCATION}_${PERSON}_${TAGS}.$EXTENSION"
+if [[ -z "$PERSON" ]]; then
+    error "Error: --usr required"
+    show_help
+    exit 9
+fi
+
+if [[ -z "$TAGS" ]]; then
+    error "Error: --tags required"
+    show_help
+    exit 10
+fi
+
+# Check remaining positional arguments
+if [[ $# == 0 ]]; then
+    error "Error: No files specified in argument list"
+    show_help
+    exit 11
+fi
+
+# Main loop
+function main() {
+  for filename in "$@"; do
+    if [[ -s "$filename" ]]; then
+        local extension="${filename##*.}"
+        local extLower=$(echo "$extension" | tr '[:upper:]' '[:lower:]')    # toLowerCase
+        local now=$(date +%Y-%m-%dT%H:%M:%S)                                # TODO Linux?
+        if [[ $EXTRACT_TS_PHOTO == true ]]; then
+            local d=$(exif --machine-readable --tag 0x9003 "$filename" 2>/dev/null)
+            if [[ -n "$d" ]]; then
+                now=$(date -j -f "%Y:%m:%d %H:%M:%S" "$d" +"%Y-%m-%dT%H:%M:%S") # TODO Linux: date -d"${d//:/-}" +"%Y-%m-%dT%H:%M:%S"
+                EXTRACT_TS_FILE=false
+                log "Extracted original photo creation time: $now"
+            else
+                echo "Cannot extract photo creation time for $filename"
+            fi
+        fi
+        if [[ $EXTRACT_TS_FILE == true ]]; then
+            now=$(stat -f %SB -t %Y-%m-%dT%H:%M:%S "$filename")             # TODO Linux?
+            log "Extracted original file creation time: $now"
+        fi
+        local year="${now:0:4}"
+        local newName="${now}_${ORIGIN}_${LOCATION}_${PERSON}_${TAGS}.$extLower"
+
+        info "Storage name: $newName"
+        sleep 1                                                             # to have diff time for diff files
+
+        mkdir -p "$SCRIPT_DIR/$year"
+        mv -v "$filename" "$SCRIPT_DIR/$year/$newName"
+        echo
+    else
+        error "Error: File missing or empty: $1" >&2
+        show_help
+        exit 12
+    fi
+  done
+}
+
+# Main program
+echo "  Files:    $@"
+echo "  --src:    $ORIGIN"
+echo "  --loc:    $LOCATION"
+echo "  --usr:    $PERSON"
+echo "  --tags:   $TAGS"
+
+main "$@"
+log "Done..."

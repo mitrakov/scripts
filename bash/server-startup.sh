1f864abfbee46a7e435544dc3a7b868e46675a44
tommy <mitrakov-artem@yandex.ru>
2025-09-24 00:14:08 +0300

    version bump

--- bash/server-startup.sh
+++ bash/server-startup.sh
@@ -10,6 +10,6 @@
 
 docker-compose up -d
 docker run --rm -d --name file-server -v /root/fileserver-data:/web -p 80:8080 halverneus/static-file-server
-docker run --rm -d --name tommyserver -e DB_PASSWORD=541888 -e SECRET_KEY=unused -p 9090:8080 mitrakov/tommy-server:1.4.17
+docker run --rm -d --name tommyserver -e DB_PASSWORD=541888 -e SECRET_KEY=unused -p 9090:8080 mitrakov/tommy-server:1.4.18
 docker run --rm -d --name tommypush -v $HOME/tommypush:/etc/tommypush mitrakov/tommypush:24.12.2
 docker run --rm -d --name openvpn --cap-add=NET_ADMIN -it -p 1194:1194/udp -p 81:8080/tcp -e HOST_ADDR=mitrakoff.com alekslitvinenk/openvpn
